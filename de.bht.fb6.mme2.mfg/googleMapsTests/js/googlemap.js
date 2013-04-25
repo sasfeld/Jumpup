e8bb2ff86b6a946f76bc03c7451b209ee6a5e470
Homer <homerjs1991@gmail.com>
2013-04-25 21:41:09 +0200

    * require works fine

--- de.bht.fb6.mme2.mfg/googleMapsTests/js/googlemap.js
+++ de.bht.fb6.mme2.mfg/googleMapsTests/js/googlemap.js
@@ -4,19 +4,16 @@ define( [ "async!http://maps.google.com/maps/api/js?key=AIzaSyBsZwdJI29OQJgyUNPb
 	/**
 	 * @param divs
 	 *          Is a assosiativ array. Avalabile options: 'map_canvas', 'textbox',
-	 *          'geocoding', 'directions'
+	 *          'geocoding'
 	 */
 	var GoogleMap = function(divs) {
-
-		console.log( google );
 
 		this.map_canvas = divs[ 'map_canvas' ];
 		this.textbox = divs[ 'textbox' ];
 		this.geocoding = divs[ 'geocoding' ];
-		this.directions = divs[ 'directions' ];
 
-		if ( !map_canvas )
-			throw "map_canvas is required for the map.";
+		if ( !this.map_canvas || !this.geocoding )
+			throw "map_canvas and geocoding is required for the map.";
 
 		this.map;
 		this.infowindow;
@@ -39,33 +36,30 @@ define( [ "async!http://maps.google.com/maps/api/js?key=AIzaSyBsZwdJI29OQJgyUNPb
 			center : torun,
 			zoom : 5
 		} );
-		google.maps.event
-				.addListener( map, 'click', function(e) {
-					geocoder
-							.geocode( {
-								'latLng' : e.latLng
-							}, function(results, status) {
-								if ( status == google.maps.GeocoderStatus.OK ) {
-									if ( results[ 0 ] ) {
-										if ( marker ) {
-											marker.setPosition( e.latLng );
-										} else {
-											marker = new google.maps.Marker( {
-												position : e.latLng,
-												map : map
-											} );
-										}
-										infowindow.setContent( results[ 0 ].formatted_address );
-										infowindow.open( map, marker );
-									} else {
-										document.getElementById( 'geocoding' ).innerHTML = 'No results found';
-									}
-								} else {
-									document.getElementById( 'geocoding' ).innerHTML = 'Geocoder failed due to: '
-											+ status;
-								}
+		google.maps.event.addListener( map, 'click', function(e) {
+			geocoder.geocode( {
+				'latLng' : e.latLng
+			}, function(results, status) {
+				if ( status == google.maps.GeocoderStatus.OK ) {
+					if ( results[ 0 ] ) {
+						if ( marker ) {
+							marker.setPosition( e.latLng );
+						} else {
+							marker = new google.maps.Marker( {
+								position : e.latLng,
+								map : map
 							} );
-				} );
+						}
+						infowindow.setContent( results[ 0 ].formatted_address );
+						infowindow.open( map, marker );
+					} else {
+						this.geocoding.innerHTML = 'No results found';
+					}
+				} else {
+					this.geocoding.innerHTML = 'Geocoder failed due to: ' + status;
+				}
+			} );
+		} );
 		this.showDirections();
 	}; // mapsLoaded()
 
@@ -75,7 +69,10 @@ define( [ "async!http://maps.google.com/maps/api/js?key=AIzaSyBsZwdJI29OQJgyUNPb
 			preserveViewport : true,
 			draggable : true
 		} );
-		directionsDisplay.setPanel( document.getElementById( 'textbox' ) );
+
+		if ( this.textbox )
+			directionsDisplay.setPanel( this.textbox );
+
 		var sampleRequest = {
 			origin : 'Warschau',
 			destination : 'Berlin, Germany',

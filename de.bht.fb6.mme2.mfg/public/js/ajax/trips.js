b7f1ca411998a3ad8ad6e9dfa43ac6a22355b470
Sascha Feldmann <sascha.feldmann@gmx.de>
2013-05-24 18:08:02 +0200

    * waypoints array

--- de.bht.fb6.mme2.mfg/public/js/ajax/trips.js
+++ de.bht.fb6.mme2.mfg/public/js/ajax/trips.js
@@ -1,105 +1,121 @@
 /*
-* Project: JumpUp
-* Realm: Frontend
-* 
-* This is the vehicle module. It is used to handle all ajax stuff for vehicles.
-*
-* gmap\handlemap.js
-* Copyright (c) 2013 Martin Schultz & Sascha Feldmann
-* license    GNU license
-* version    1.0
-* since      05.05.2013
+ * Project: JumpUp
+ * Realm: Frontend
+ * 
+ * This is the vehicle module. It is used to handle all ajax stuff for vehicles.
+ *
+ * gmap\handlemap.js
+ * Copyright (c) 2013 Martin Schultz & Sascha Feldmann
+ * license    GNU license
+ * version    1.0
+ * since      05.05.2013
  */
 
-define(["jquery"], 
+define(
+		[ "jquery" ],
 		(function($) {
 			var mapCtrl;
-			
+
 			/*
-			 * Create a new TripsController.
-			 * - param options an array:
-			 * 	 getTripsUrl - the url to the json endpoint for the listing of all vehicles
-			 * 	 mapCtrl - the map controller
+			 * Create a new TripsController. - param options an array:
+			 * getTripsUrl - the url to the json endpoint for the listing of all
+			 * vehicles mapCtrl - the map controller
 			 */
 			var TripsController = function(options) {
 				this.options = options;
 				mapCtrl = options.mapCtrl;
 			};
-			
+
 			/*
 			 * HandleServerResponse will be called after a successfull request.
 			 * @param data
 			 */
-			TripsController.prototype.handleServerResponse = function(data) {	
+			TripsController.prototype.handleServerResponse = function(data) {
 				// bad request?
 				console.log(data);
 				// inform gui
-				if(data.trips.length > 0) {
-					for(var tripIndex = 0; tripIndex < data.trips.length; tripIndex++) {
+				if (data.trips.length > 0) {
+					for ( var tripIndex = 0; tripIndex < data.trips.length; tripIndex++) {
 						var trip = data.trips[tripIndex];
 						var id = trip.id;
 						var startPoint = trip.startPoint;
 						var endPoint = trip.endPoint;
 						var startDate = trip.startDate;
 						var price = trip.price;
-						var driver = trip.driver; // currently: prename and lastname
+						var driver = trip.driver; // currently: prename and
+													// lastname
 						var startCoord = trip.startCoord;
 						var endCoord = trip.endCoord;
 						var overviewPath = trip.overviewPath;
 						var viaWaypoints = trip.viaWaypoints;
 						var numberBookings = trip.numberBookings;
 						var maxSeats = trip.maxSeats;
-						
+
+						var waypointsArray = new Array();
+						if (viaWaypoints != null) {
+							waypointsArray = viaWaypoints.split(";");
+							waypointsArray.pop(); // last empty element
+							for ( var i = 0; i < waypointsArray.length; i++) {
+								waypointsArray[i] = "(" + waypointsArray[i]
+										+ ")";
+							}
+							console.log("trips.js: waypoints array: "
+									+ waypointsArray);
+						}
 						// TODO get multiple routes working.
-						if(null != startCoord && null != endCoord) {
-							mapCtrl.showRoute(startCoord, endCoord, null, true);
+						if (null != startCoord && null != endCoord) {
+							mapCtrl.showRoute(startCoord, endCoord,
+									waypointsArray, true);
 						}
 						// TODO build selection view for user
-						
-					}; 
-					
-				};
+
+					}
+					;
+
+				}
+				;
 			};
-			
+
 			/*
 			 * Error-Event method if the ajax request below fails.
 			 */
-			TripsController.prototype.handleError = function(xhr, ajaxOptions, thrownError) {
+			TripsController.prototype.handleError = function(xhr, ajaxOptions,
+					thrownError) {
 				console.log("TripsController: handleError");
 				console.log(xhr);
 			};
-			
+
 			/*
 			 * Fetch the trips to a given id.
 			 */
-			TripsController.prototype.fetchTrips = function(startCoord, endCoord, dateFrom, dateTo, priceFrom, priceTo) {
+			TripsController.prototype.fetchTrips = function(startCoord,
+					endCoord, dateFrom, dateTo, priceFrom, priceTo) {
 				console.log("TripsController: fetchTrips");
-				console.log("startCoord: "+startCoord);
-				console.log("endCoord: "+endCoord);
-				console.log("startDate: "+startDate);
-				console.log("endDate: "+endDate);
-				console.log("priceFrom: "+priceFrom);
-				console.log("priceTo: "+priceTo);
-				
-				$.ajax( {
-					url: this.options.getTripsUrl,
-					data: {
-						"startCoord" : startCoord,						
-						"endCoord" : endCoord,						
-						"startDate" : startDate,						
-						"endDate" : endDate,						
-						"priceFrom" : priceFrom,						
-						"priceTo" : priceTo,						
+				console.log("startCoord: " + startCoord);
+				console.log("endCoord: " + endCoord);
+				console.log("startDate: " + startDate);
+				console.log("endDate: " + endDate);
+				console.log("priceFrom: " + priceFrom);
+				console.log("priceTo: " + priceTo);
+
+				$.ajax({
+					url : this.options.getTripsUrl,
+					data : {
+						"startCoord" : startCoord,
+						"endCoord" : endCoord,
+						"startDate" : startDate,
+						"endDate" : endDate,
+						"priceFrom" : priceFrom,
+						"priceTo" : priceTo,
 					},
-					dataType: 'json',
-					type: "POST",
-					success: this.handleServerResponse,
-					error: this.handleError,
+					dataType : 'json',
+					type : "POST",
+					success : this.handleServerResponse,
+					error : this.handleError,
 				});
 			};
-			
-			
-			return TripsController;			
+
+			return TripsController;
 		})
-		
+
 );

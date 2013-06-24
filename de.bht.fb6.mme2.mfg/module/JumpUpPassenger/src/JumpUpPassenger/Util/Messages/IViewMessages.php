16d8bad2e8aba9341284ec3646b3dcb460373440
Sascha Feldmann <sascha.feldmann@gmx.de>
2013-06-24 17:00:18 +0200

    JumpUpPassenger: views -> moved messages to IViewMessages

---
+++ de.bht.fb6.mme2.mfg/module/JumpUpPassenger/src/JumpUpPassenger/Util/Messages/IViewMessages.php
@@ -0,0 +1,41 @@
+<?php
+namespace JumpUpPassenger\Util\Messages;
+
+
+
+/**
+ * 
+* Interface which only contains messages for the view which are called by view files (*.phtml).
+*
+* @package    JumpUpPassenger\Util
+* @subpackage 
+* @copyright  Copyright (c) 2013 Sascha Feldmann (http://saschafeldmann.de)
+* @license    GNU license
+* @version    1.0
+* @since      24.06.2013
+ */
+interface IViewMessages {
+    
+    
+    /* ..:: used in booking\.*.phtml ::../
+     *
+    */
+   const VIEW_BOOKINGS_TITLE = "View bookings";
+   const BOOK_TRIP_TITLE = "Book trip";
+   const BOOKING_ERROR = "Booking system error";
+   	/*
+     * ..:::::::::::::::::::::::::::::::::..
+    */
+    /* ..:: used in view-trips\.*.phtml ::../
+     *
+    */
+   const LOOK_UP_TITLE = "Look for trips";
+   /**
+    * @deprecated because showTrips is not used
+    */
+   const SHOW_TRIPS_TITLE = "Show trips";
+   	/*
+     * ..:::::::::::::::::::::::::::::::::..
+    */
+  
+}
